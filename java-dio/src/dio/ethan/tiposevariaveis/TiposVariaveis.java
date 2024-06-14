4bf060d9dfff55dec0b60c0cb5bb23df566c1d76
Ethan Daniel <dansilveira383@gmail.com>
2024-06-14 14:43:40 -0300

    Exemplos de conceitos de tipos e variaves

--- java-dio/src/dio/ethan/tiposevariaveis/TiposVariaveis.java
+++ java-dio/src/dio/ethan/tiposevariaveis/TiposVariaveis.java
@@ -1,7 +1,21 @@
 package dio.ethan.tiposevariaveis;
 
 public class TiposVariaveis {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws Exception {
+        //dois mil e quinhentos; 2.500 = dois virgula cinco
+        double salarioMinimo = 2500;
+
+        //casting = mudando para um tipo mais especifico
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short)numeroNormal;
+
+        //variavel mutavel
+        int numero = 5;
+        numero = 10;
+        System.out.print(numero);
+
+        //variavel imutavel
+        final double VALOR_DE_PI = 3.14;
     }
 }
