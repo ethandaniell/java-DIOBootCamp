4158f07d9e24d3ee782033ca0e0a7fb88ce78768
Ethan Daniel <dansilveira383@gmail.com>
2024-06-17 15:56:42 -0300

    exemplo de concatenação

---
+++ java-dio/src/dio/ethan/operadores/Concatenacao.java
@@ -0,0 +1,9 @@
+package dio.ethan.operadores;
+
+public class Concatenacao {
+    public static void main(String[] args) {
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+
+        System.out.println(nomeCompleto);
+    }
+}
