7150880ae979108e0964be657b3bcc30357d2aa6
Ethan Daniel <dansilveira383@gmail.com>
2024-06-17 16:07:02 -0300

    exemplo de operadores unitarios

---
+++ java-dio/src/dio/ethan/operadores/Unitarios.java
@@ -0,0 +1,13 @@
+package dio.ethan.operadores;
+
+public class Unitarios {
+    public static void main(String[] args) {
+        int numero = 5;
+        //transforma em negativo
+        numero = - numero;
+        System.out.println(numero);
+        //transforma em positivo de novo
+        numero = numero * -1;
+        System.out.println(numero);
+    }
+}
