b96836f7abdfbb99b84766d060f7be2f61f71d09
Ethan Daniel <dansilveira383@gmail.com>
2024-06-17 16:13:34 -0300

    exmplo de incrementação de valores

---
+++ java-dio/src/dio/ethan/operadores/IncrementarValores.java
@@ -0,0 +1,10 @@
+package dio.ethan.operadores;
+
+public class IncrementarValores {
+    public static void main(String[] args) {
+        int numero = 5;
+        //x repetição
+        //numero++; numero = numero + 1
+        System.out.println(++ numero); //coloca o ++ antes para ele apresentar direto o numero somado
+    }
+}
