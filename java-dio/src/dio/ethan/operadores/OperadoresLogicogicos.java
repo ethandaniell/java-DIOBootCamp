a50e4ce6f18d66fb353fcd745fbe1eb14278865d
Ethan Daniel <dansilveira383@gmail.com>
2024-06-17 16:31:37 -0300

    exemplo de operador logico

---
+++ java-dio/src/dio/ethan/operadores/OperadoresLogicogicos.java
@@ -0,0 +1,16 @@
+package dio.ethan.operadores;
+
+public class OperadoresLogicogicos {
+    public static void main(String[] args) {
+        boolean condicao1 = true;
+        boolean condicao2 = true;
+
+        if(condicao1 && (7 > 4)) {
+            System.out.println("As duas são verdadeiras");
+        }
+        if(condicao1 || condicao2) {
+            System.out.println("Uma das condições é verdadeira");
+        }
+        System.out.println("fim");
+    }
+}
