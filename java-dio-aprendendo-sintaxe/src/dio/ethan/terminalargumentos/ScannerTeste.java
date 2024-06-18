663aa446df4cbf60753b885b7de01ad7b699e70b
Ethan Daniel <dansilveira383@gmail.com>
2024-06-18 11:59:52 -0300

    scanner

---
+++ java-dio-aprendendo-sintaxe/src/dio/ethan/terminalargumentos/ScannerTeste.java
@@ -0,0 +1,28 @@
+package dio.ethan.terminalargumentos;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ScannerTeste {
+    public static void main(String[] args) {
+        //criando o objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu nome");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite sua altura");
+        double altura = scanner.nextDouble();
+
+        //imprimindo os dados pelo usuário
+        System.out.println("Ola, me chamo " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos ");
+        System.out.println("Minha altura é " + altura + "cm ");
+    }
+}
