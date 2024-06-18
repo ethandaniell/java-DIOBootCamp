c0051db1bc82083405ccc7e3c1c9b2e781415d79
Ethan Daniel <dansilveira383@gmail.com>
2024-06-18 11:07:41 -0300

    exemplos de usos com os documentos java

---
+++ java-dio/src/dio/ethan/javadoc/Calculadora.java
@@ -0,0 +1,20 @@
+package dio.ethan.javadoc;
+/** 
+* @author Prof.Gleyson Sampaio
+* @version 1.0
+* @since 01/01/2022
+*/
+
+public class Calculadora {
+    /** 
+     * Este metodo é utilizado para somar dois numeros inteiros
+     * @param numeroUm
+     * @param numeroDois
+     * @return
+    */
+
+    public int somar(int numeroUm, int numeroDois) {
+        return numeroUm + numeroDois;
+    }
+    
+}
---
+++ java-dio/src/dio/ethan/javadoc/Comentarios.java
@@ -0,0 +1,20 @@
+package dio.ethan.javadoc;
+
+public class Comentarios {
+    public static void main(String[] args) {
+        //eu sou um comentario em uma unica linha
+
+        /* Ola,
+         * Eu sou um comentario
+         * que pode ser masi detalhado
+         * quando necessario
+         */
+
+         /**
+          * Estas duas estrelinhas acima
+          * é para identificar que você
+          * pretende elaborar um comentario
+          * a nivel de documentação
+          */
+    }
+}
