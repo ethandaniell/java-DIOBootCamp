e35c881943c229783cf1f45a397a1afdc8924e68
Ethan Daniel <dansilveira383@gmail.com>
2024-06-20 14:14:58 -0300

    exemplo de depuração

--- debugging-java/src/dio/ethan/debugging/CalculadoraDeMedias.java
+++ debugging-java/src/dio/ethan/debugging/CalculadoraDeMedias.java
@@ -1,5 +1,26 @@
 package dio.ethan.debugging;
 
+import java.util.Scanner;
+
 public class CalculadoraDeMedias {
-    
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        String[] alunos = {"Camila", "Lucas", "Bruna", "Pedro"};
+
+        double media = calculoMediaDaTurma(alunos, scan);
+
+        System.out.printf("Média da turma %.1f", media);
+    }
+
+    public static double calculoMediaDaTurma(String[] alunos, Scanner scanner) {
+        
+        double soma = 0;
+        for(String aluno : alunos) {
+            System.out.printf("Nota do aluno %s: ", aluno);
+            double nota = scanner.nextDouble();
+            soma += nota;
+        }
+        return soma / alunos.length;
+    }
 }
