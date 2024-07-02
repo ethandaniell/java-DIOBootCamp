b8eb8ff4d5d60ce213dc01c2b1f59f9fb5ff82f3
Ethan Daniel <dansilveira383@gmail.com>
2024-07-02 17:32:12 -0300

    atividade carrinho de compras

--- 07.collections-java/src/dio/ethan/List/OperacoesBasicas/CarrinhoDeCompras.java
+++ 07.collections-java/src/dio/ethan/List/OperacoesBasicas/CarrinhoDeCompras.java
@@ -1,3 +1,68 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class CarrinhoDeCompras {
-    
+    private List<Item> itemList;
+
+    public CarrinhoDeCompras() {
+        this.itemList = new ArrayList<>();
+    }
+
+    //metodos
+    public void adicionarItem(String nome, double preco, int quantidade) {
+        Item item = new Item(nome, preco, quantidade);
+        this.itemList.add(item);
+    }
+
+    public void removerItem(String nome) {
+        List <Item> itemParaRemover = new ArrayList<>();
+        if(!itemList.isEmpty()) {
+            for(Item i : itemList) {
+                if(i.getNome().equalsIgnoreCase(nome)) {
+                    itemParaRemover.add(i);
+                }
+            }
+            itemList.removeAll(itemParaRemover);
+        } else {
+            System.out.println("A lista esta vazia!");
+        }
+    }
+
+    public double calcularValorTotal() {
+        double valortotal = 0d;
+        if(!itemList.isEmpty()) {
+            for(Item item : itemList) {
+                double valorItem = item.getPreco() * item.getQuantidade();
+                valortotal += valorItem;
+            }
+            return valortotal;
+        } else {
+            throw new RuntimeException("A lista esta vazia");
+        }
+    }
+
+    public void exibirItens() {
+        if(!itemList.isEmpty()) {
+            System.out.println(this.itemList);
+        } else {
+            System.out.println("A lista esta vazia!");
+        }
+    }
+
+    public static void main(String[] args) {
+        CarrinhoDeCompras carrinhoDeCompras = new CarrinhoDeCompras();
+
+        carrinhoDeCompras.adicionarItem("Lapis", 2d, 3);
+        carrinhoDeCompras.adicionarItem("Lapis", 2d, 3);
+        carrinhoDeCompras.adicionarItem("Caderno", 35d, 1);
+        carrinhoDeCompras.adicionarItem("Cola", 2d, 2);
+
+        carrinhoDeCompras.exibirItens();
+        
+        carrinhoDeCompras.removerItem("Lapis");
+
+        carrinhoDeCompras.exibirItens();
+        
+        System.out.println("O valor total da compra é " + carrinhoDeCompras.calcularValorTotal());
+    }
 }
--- 07.collections-java/src/dio/ethan/List/OperacoesBasicas/Item.java
+++ 07.collections-java/src/dio/ethan/List/OperacoesBasicas/Item.java
@@ -1,3 +1,34 @@
 public class Item {
-    
+    //atributos
+    private String nome;
+    private double preco;
+    private int quantidade;
+
+    //cria um objeto do tipo item
+    public Item(String nome, double preco, int quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    //metodo get
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return "Item: " +
+				"nome = '" + nome + '\'' +
+				", preco = " + preco +
+				", quant = " + quantidade;
+    }
 }
