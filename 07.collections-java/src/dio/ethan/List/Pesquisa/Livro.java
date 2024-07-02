1c9c1d426b7513ce2b25c10a0080c80510ac847d
Ethan Daniel <dansilveira383@gmail.com>
2024-07-02 18:24:03 -0300

    exercicio catalogo de livros

---
+++ 07.collections-java/src/dio/ethan/List/Pesquisa/Livro.java
@@ -0,0 +1,34 @@
+public class Livro {
+    //atributos
+    private String titulo;
+    private String autor;
+    private int anoPublicacao;
+
+    //contrutor
+    public Livro(String titulo, String autor, int anoPublicacao) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    //metodo get 
+    public String geTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro: " +
+                    "titulo = `" + titulo + '\'' +
+                    ", autor = `" + autor  +
+                    ", ano publicacao = `" + anoPublicacao;
+    }
+}
