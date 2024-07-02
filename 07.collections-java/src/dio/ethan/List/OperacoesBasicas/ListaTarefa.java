51dacbaed2c6800d0c557e440a828e819ba404ed
Ethan Daniel <dansilveira383@gmail.com>
2024-07-02 16:37:00 -0300

    primeira atividade lista de tarefas

---
+++ 07.collections-java/src/dio/ethan/List/OperacoesBasicas/ListaTarefa.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+   //atributo com generics
+   private List<Tarefa> tarefaList;
+
+   //construtor
+   public ListaTarefa() {
+        this.tarefaList = new ArrayList<>();
+   }
+
+   //metodos
+   public void adicionarTarefa(String descricao) {
+        tarefaList.add(new Tarefa(descricao));
+   }
+
+   public void removerTarefa(String descricao) {
+        //analisa se tem descrições repetidas
+        List <Tarefa> tarefaParaRemover = new ArrayList<>();
+        for(Tarefa t : tarefaList) {
+            if(t.getDescricao().equalsIgnoreCase(descricao)) {
+                tarefaParaRemover.add(t);
+            }
+        }
+        tarefaList.removeAll(tarefaParaRemover);
+   }
+
+   public int obterNumeroTotalTarefas() {
+        return tarefaList.size();
+   }
+
+   public void obterDescricoesTarefas() {
+        System.out.println(tarefaList);
+   }
+
+   public static void main(String[] args) {
+        //uma instancia da classe lista tarefa
+        ListaTarefa listaTarefa = new ListaTarefa();
+
+        //adicionando tarefas a lista
+        listaTarefa.adicionarTarefa("Comprar leite");
+        listaTarefa.adicionarTarefa("Estudar para a prova");
+        listaTarefa.adicionarTarefa("Fazer ativiadades");
+        listaTarefa.adicionarTarefa("Trabalhar");
+        
+        //exibindo o numero total de tarefas
+        System.out.println("Você tem " + listaTarefa.obterNumeroTotalTarefas() + "tarefas na lista");
+
+        //exibindo as descriçoes das tarefas
+        listaTarefa.obterDescricoesTarefas();
+
+        //removendo uma tarefa da lista
+        listaTarefa.removerTarefa("Trabalhar");
+   }
+}
---
+++ 07.collections-java/src/dio/ethan/List/OperacoesBasicas/Tarefa.java
@@ -0,0 +1,20 @@
+public class Tarefa {
+    //atributo
+    private String descricao;
+
+    //construtor que cria um objeto do tipo tarefa
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    //metodo get
+    public String getDescricao() {
+        return descricao;
+    }
+
+    //pra poder chamar o metodo tarefa e aparecer as descriçoes
+    @Override
+    public String toString() {
+        return descricao;
+    }
+}
