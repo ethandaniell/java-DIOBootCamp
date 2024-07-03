76093f8ef7a226933a743d74d210c202c3d1a412
Ethan Daniel <dansilveira383@gmail.com>
2024-07-03 18:29:29 -0300

    atividades pesquisa map

---
+++ 07.collections-java/src/dio/ethan/Map/Pesquisa/ContagemPalavras.java
@@ -0,0 +1,48 @@
+package dio.ethan.Map.Pesquisa;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ContagemPalavras {
+    private Map<String, Integer> palavras;
+
+    public ContagemPalavras() {
+        this.palavras = new HashMap<>();
+    }
+
+    public void adicionarPalavras(String palavra, Integer contagem) {
+        palavras.put(palavra, contagem);
+    }
+
+    public void removerPalavra(String palavra) {
+        palavras.remove(palavra);
+    }
+
+    public void exibirContagemPalavras() {
+        System.out.println(palavras);
+    }
+
+    public String encontrarPalavrasMaisFrequente() {
+        String linguagemMaisFrequente = null;
+        int maiorContagem = 0;
+        for(Map.Entry<String, Integer> entry : palavras.entrySet()) {
+            if(entry.getValue() > maiorContagem) {
+                maiorContagem = entry.getValue();
+                linguagemMaisFrequente = entry.getKey();
+            }
+        }
+        return linguagemMaisFrequente;
+    }
+
+    public static void main(String[] args) {
+        ContagemPalavras contagemLinguagens = new ContagemPalavras();
+
+        contagemLinguagens.adicionarPalavras("Java", 2);
+        contagemLinguagens.adicionarPalavras("Python", 8);
+        contagemLinguagens.adicionarPalavras("JavaScript", 1);
+        contagemLinguagens.adicionarPalavras("C#", 6);
+
+        String linguagemMaisFrequente = contagemLinguagens.encontrarPalavrasMaisFrequente();
+        System.out.println("A linguagem mais frequente é: " + linguagemMaisFrequente);
+    }
+}
---
+++ 07.collections-java/src/dio/ethan/Map/Pesquisa/Produto.java
@@ -0,0 +1,34 @@
+package dio.ethan.Map.Pesquisa;
+
+public class Produto {
+    private String nome;
+    private double preco;
+    private int quantidade;
+
+    public Produto(String nome, double preco, int quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto: " +
+            " nome = '" + getNome() + "'" +
+            ", preco = '" + getPreco() + "'" +
+            ", quantidade = '" + getQuantidade() + "'";
+    }
+
+}
