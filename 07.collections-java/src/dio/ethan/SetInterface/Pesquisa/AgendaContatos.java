fecbfc1f37310ff6da76c8e33e001104b3714cb5
Ethan Daniel <dansilveira383@gmail.com>
2024-07-03 12:09:41 -0300

    atividade agenda de contatos set

---
+++ 07.collections-java/src/dio/ethan/SetInterface/Pesquisa/AgendaContatos.java
@@ -0,0 +1,59 @@
+package dio.ethan.SetInterface.Pesquisa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class AgendaContatos {
+    private Set<Contato> contatosSet;
+
+    public AgendaContatos() {
+        this.contatosSet = new HashSet<>();
+    }
+
+    public void adicionarContato(String nome, int telefone) {
+        contatosSet.add(new Contato(nome, telefone));
+    }
+
+    public void exibirContatos() {
+        System.out.println(contatosSet);
+    }
+
+    public Set<Contato> pesquisarPorNome(String nome) {
+        Set<Contato> contatosPorNome = new HashSet<>();
+        for(Contato c : contatosSet) {
+            if(c.getNome().startsWith(nome)) {
+                contatosPorNome.add(c);
+            }
+        }
+        return contatosPorNome;
+    }
+
+    public Contato atualizarNumeroContato(String nome, int novoNumero) {
+        Contato contatoAtualizado = null;
+        for(Contato c : contatosSet) {
+            if(c.getNome().equalsIgnoreCase(nome)) {
+                c.setTelefone(novoNumero);
+                contatoAtualizado = c;
+                break;
+            }
+        }
+        return contatoAtualizado;
+    }
+
+    public static void main(String[] args) {
+        AgendaContatos agendaContatos = new AgendaContatos();
+
+        agendaContatos.adicionarContato("João", 123456789);
+        agendaContatos.adicionarContato("Maria", 987654321);
+        agendaContatos.adicionarContato("Maria Fernandes", 55555555);
+        agendaContatos.adicionarContato("Ana", 88889999);
+        agendaContatos.adicionarContato("Fernando", 77778888);
+        agendaContatos.adicionarContato("Carolina", 55555555);
+
+        agendaContatos.exibirContatos();
+
+        agendaContatos.pesquisarPorNome("Maria");
+
+        agendaContatos.atualizarNumeroContato("Carolina", 44443333);
+    }
+}
---
+++ 07.collections-java/src/dio/ethan/SetInterface/Pesquisa/Contato.java
@@ -0,0 +1,46 @@
+package dio.ethan.SetInterface.Pesquisa;
+
+import java.util.Objects;
+
+public class Contato {
+    //atributo
+    private String nome;
+    private int telefone;
+
+    //construtor
+    public Contato(String nome, int telefone) {
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Contato contato)) return false;
+        return Objects.equals(getNome(), contato.getNome());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getNome());
+    }
+
+    @Override
+    public String toString() {
+        return "{" + nome + "," + telefone + "}";
+    }
+
+}
